5c542e7fbbb1fedeff451d300de0ddec8e67bf49
swapneil <Swapneil.dash2@gmail.com>
2020-01-07 21:55:58 +0530

    Add files via upload

---
+++ slack_api.py
@@ -0,0 +1,17 @@
+from slacker import Slacker
+import base64
+
+slack_api_token_base64= "eG94cC04NTQzMDMxNDIwNTUtODU0MzAzMTQyOTUxLTg1NTM1MzgwNzA2Mi0yMDY5YTU0M2I1MjQ1MjBkOTBiMmEzMjEwOTNjMDY3Mg=="
+slack_api_token= base64.b64decode(slack_api_token_base64)
+slack = Slacker(slack_api_token)
+if slack.api.test().successful:
+    print(
+        f"Connected to {slack.team.info().body['team']['name']}.")
+else:
+    print('Try Again!')
+
+r = slack.channels.list()
+channels = r.body
+# Iterate through channels
+for c in channels['channels']:
+    print(f'Channel {c["name"]} Purpose: {c["purpose"]["value"]}')
